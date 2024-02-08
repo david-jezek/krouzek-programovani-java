2c07b499be3c5227a84421534157640b1851ab5d
Matthias <mmatthias00701@gmail.com>
2024-02-08 17:33:21 +0100

    asi hotovo

--- wild-world/src/main/java/cz/vsb/fei/kp/wildworld/Weapon.java
+++ wild-world/src/main/java/cz/vsb/fei/kp/wildworld/Weapon.java
@@ -4,95 +4,96 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
 import java.awt.geom.Point2D.Double;
+import java.math.MathContext;
 
 public class Weapon extends Sprite {
-	private double playerDirection;
+	private double playerDirection = 0;
+
 	public Weapon() {
 		super("/weapon.png");
 	}
 
 	public void swing(double startDir, double endDir) {
-		Action attack = new SwingAction(startDir, endDir);
+		Action attack = new SwingAction(startDir, 90);
 		addAction(attack);
 	}
+
 	public double checkDirection() {
-		Action checkDirection = new DirectionCheckAction();
-		addAction(checkDirection);
-		return playerDirection;
-	}
-	private class DirectionCheckAction extends Action{
-		@Override
-		public boolean doConcreteAction() {
+		if (getWorld().isKeyPressed(KeyEvent.VK_W)) {
+			playerDirection = 270;
+			if (getWorld().isKeyPressed(KeyEvent.VK_A)) {
+				playerDirection = 225;
+			} else if (getWorld().isKeyPressed(KeyEvent.VK_D)) {
+				playerDirection = 315;
+			}
+		} else if (getWorld().isKeyPressed(KeyEvent.VK_A)) {
 			playerDirection = 180;
 			if (getWorld().isKeyPressed(KeyEvent.VK_W)) {
-				playerDirection = 270;
-				if (getWorld().isKeyPressed(KeyEvent.VK_A)) {
-					playerDirection = 225;
-				}
-				else if (getWorld().isKeyPressed(KeyEvent.VK_D)) {
-					playerDirection = 315;
-				}
+				playerDirection = 225;
+			} else if (getWorld().isKeyPressed(KeyEvent.VK_S)) {
+				playerDirection = 135;
 			}
-			else if (getWorld().isKeyPressed(KeyEvent.VK_A)) {
-				playerDirection = 180;
-				if (getWorld().isKeyPressed(KeyEvent.VK_W)) {
-					playerDirection = 225;
-				}
-				else if (getWorld().isKeyPressed(KeyEvent.VK_S)) {
-					playerDirection = 135;
-				}
+		} else if (getWorld().isKeyPressed(KeyEvent.VK_S)) {
+			playerDirection = 90;
+			if (getWorld().isKeyPressed(KeyEvent.VK_A)) {
+				playerDirection = 135;
+			} else if (getWorld().isKeyPressed(KeyEvent.VK_D)) {
+				playerDirection = 45;
 			}
-			else if (getWorld().isKeyPressed(KeyEvent.VK_S)) {
-				playerDirection = 90;
-				if (getWorld().isKeyPressed(KeyEvent.VK_A)) {
-					playerDirection = 135;
-				}
-				else if (getWorld().isKeyPressed(KeyEvent.VK_D)) {
-					playerDirection = 45;
-				}
+		} else if (getWorld().isKeyPressed(KeyEvent.VK_D)) {
+			playerDirection = 0;
+			if (getWorld().isKeyPressed(KeyEvent.VK_W)) {
+				playerDirection = 315;
+			} else if (getWorld().isKeyPressed(KeyEvent.VK_S)) {
+				playerDirection = 45;
 			}
-			else if (getWorld().isKeyPressed(KeyEvent.VK_D)) {
-				playerDirection = 0;
-				if (getWorld().isKeyPressed(KeyEvent.VK_W)) {
-					playerDirection = 315;
-				}
-				else if (getWorld().isKeyPressed(KeyEvent.VK_S)) {
-					playerDirection = 45;
-				}
-			}
-			else {
-				playerDirection = 0;
-			}
-			return false;
 		}
-		
+		return playerDirection;
 	}
+
 	private class SwingAction extends Action {
 		private double startDirection;
+		private double diffDirection;
 		private double rotationSpeed = 10;
-		private double endDirection;
-		private double speed;
-		private boolean isReturning = false; 
+		private boolean isReturning = false;
 
-		public SwingAction(double startDirection, double endDirection) {
+		public SwingAction(double startDirection, double diffDirection) {
 			super();
-			this.startDirection=startDirection;
-			this.endDirection = endDirection;
+			this.startDirection = startDirection;
+			this.diffDirection = diffDirection;
 		}
+
 		@Override
 		public boolean doConcreteAction() {
-				if (!isReturning && Math.abs(getDirection() - endDirection) <= Math.abs(rotationSpeed)) {
-					setDirection(endDirection);
-					rotationSpeed = - rotationSpeed;
+			// playerDirection != 0 && playerDirection != 45 && playerDirection != 90 &&
+			// playerDirection != 315
+			int d = (int) checkDirection();
+			if (d != 0 && d != 45 && d != 90 && d != 315 && !isReturning) {
+				rotationSpeed = -10;
+				if (!isReturning && Math.abs(getDirection() - (startDirection-diffDirection)) <= (Math.abs(rotationSpeed))) {
+					setDirection(startDirection-diffDirection);
+					rotationSpeed = -rotationSpeed;
 					isReturning = true;
-//					finish();
-				} else if(isReturning && Math.abs(startDirection - getDirection()) <= Math.abs(rotationSpeed)) {
+
+				} else if (isReturning && Math.abs(startDirection - getDirection()) <= (Math.abs(rotationSpeed))) {
 					setDirection(startDirection);
 					rotationSpeed = 0;
 					finish();
 				} else {
 					setDirection(getDirection() + rotationSpeed);
 				}
+			} else if (!isReturning && Math.abs(getDirection() - (startDirection+diffDirection)) <= Math.abs(rotationSpeed)) {
+				setDirection(startDirection+diffDirection);
+				rotationSpeed = -rotationSpeed;
+				isReturning = true;
+
+			} else if (isReturning && Math.abs(startDirection - getDirection()) <= Math.abs(rotationSpeed)) {
+				setDirection(startDirection);
+				rotationSpeed = 0;
+				finish();
+			} else {
+				setDirection(getDirection() + rotationSpeed);
+			}
 			return false;
 		}
 	}
